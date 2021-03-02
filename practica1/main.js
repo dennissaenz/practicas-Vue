889732b746ab300c96ec96a13e4cb9482afa7cd7
Dennis Oswaldo Saenz Hernandez <3385806-dsaenz1@users.noreply.gitlab.com>
2021-03-01 20:20:35 -0700

    practica 1 subida
    hola mundo en vue

---
+++ practica1/main.js
@@ -0,0 +1,12 @@
+
+const app = Vue.createApp({
+    data(){
+        return{
+            titulo:"Mi Banco",
+            saludo:"Saludos desde main js",
+            cantidad: 1500
+        }
+    }
+});
+
+//console.log("la version es" +Vue.version );
